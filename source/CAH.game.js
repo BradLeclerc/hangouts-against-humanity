5c7f8248e3d48d68dc54a889b5bd02a0a866536e
Brad Leclerc <brad.leclerc@gmail.com>
2014-05-19 18:43:31 -0400

    fixing typo and comma

--- source/CAH.game.js
+++ source/CAH.game.js
@@ -58,7 +58,7 @@ function startGame() {
                         { boxLabel: 'Cards Against Gallifrey (fan Doctor Who set)', name: 'sets', checked: false, inputValue: 'Gallifrey'},
 			{ boxLabel: 'Cards Against Alternia (fan Homestuck set)', name: 'sets', checked: false, inputValue: 'Alternia'},
 			{ boxLabel: 'Ladies Against Humanity', name: 'sets', checked: false, inputValue: 'Ladies Against Humanity'},
-			{ boxLabel: 'Canadian Conversion', name: 'sets', checked: false, inputValue: 'Canadian Conversion'},
+			{ boxLabel: 'Canadian Conversion Kit', name: 'sets', checked: false, inputValue: 'Canadian Conversion Kit'}
                     ]
                 },
                 {
